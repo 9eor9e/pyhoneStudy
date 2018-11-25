4536f4b236a026a732cb47febbd0128a9c8bea95
9eor9e <boneizo@gmail.com>
2018-11-25 19:47:19 +0800

    继续跟着书籍学习python。

---
+++ 0004_strip.py
@@ -0,0 +1,12 @@
+#strip方法的使用。
+#lstrip可以去除字符串左边的空格。
+#rstrip可以去除字符串右边的空格。
+#strip可以去除字符串左右两边的空格。
+
+name_1 = 'ada loveada '
+name_2 = ' ada loveada'
+name_3 = ' ada loveada '
+
+print(name_1.rstrip())
+print(name_2.lstrip())
+print(name_3.strip())
