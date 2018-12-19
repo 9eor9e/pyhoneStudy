3d1df92c39191105c74b96983bc12aa6518e3863
9eor9e <boneizo@gmail.com>
2018-12-19 22:38:36 +0800

    input()函数和%求模运算符练习

--- 0045_input()_practice_1.py
+++ 0045_input()_practice_1.py
@@ -13,7 +13,7 @@
 # input()函数和%求模运算符 - 练习3
 number = input("Please enter a number, I can tell you the number 是不是整数倍。")
 number = int(number)
-if number % 10 ==0:
+if number % 10 == 0:
     print("This number 是10的整数倍！")
 else:
     print("This number is not 10的整数倍。")
