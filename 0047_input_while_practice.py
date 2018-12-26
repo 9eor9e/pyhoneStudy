24af2ecc16c7e7c408947be0aa06077175340de6
9eor9e <boneizo@gmail.com>
2018-12-26 19:58:46 +0800

    input()和while循环练习

--- 0047_input_while_practice.py
+++ 0047_input_while_practice.py
@@ -9,14 +9,34 @@
 #         print(topping.title() + " will add!")
 
 # input()和while循环 - 练习2
+# prompt = "\nPlease enter your age,thanks."
+# prompt += "\nWe will tell you how much the ticket!"
+# age = input(prompt)
+# age = int(age)
+# while True:
+#     if age < 3 :
+#         print("It's free for you!")
+#         break
+#     elif age <= 12 :
+#         print("Please pay 10$ .")
+#         break
+#     else :
+#         print("Please pay 15$ .")
+#         break
+
+# input()和while循环 - 练习3
 prompt = "\nPlease enter your age,thanks."
 prompt += "\nWe will tell you how much the ticket!"
-while True:
-    age = input(prompt)
-    age = int(age)
+age = input(prompt)
+age = int(age)
+active = True
+while active:
     if age < 3 :
         print("It's free for you!")
+        active = False
     elif age <= 12 :
         print("Please pay 10$ .")
+        active = False
     else :
         print("Please pay 15$ .")
+        active = False
